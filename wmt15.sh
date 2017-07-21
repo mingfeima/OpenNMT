1a7c8ff230fda67e415740e007ea62be6d9ff688
Mingfei Ma <mingfei.ma@intel.com>
2017-07-21 10:18:54 +0800

    update wmt15.sh

--- wmt15.sh
+++ wmt15.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 path=data/wmt15-de-en
-nparallel=4
-lr=0.006
+nparallel=2
+lr=0.0064
 optim=adam
 log=wmt15_np${nparallel}_lr${lr}.txt
 
-OMP_NUM_THREADS=72 th train.lua -data $path/wmt15-all-en-de-train.t7 -save_model $path/wmt15-all-en-de -nparallel $nparallel -learning_rate $lr -curriculum 1 -optim $optim -start_decay_ppl_delta 2
+th train.lua -data $path/wmt15-all-en-de-train.t7 -save_model $path/wmt15-all-en-de -nparallel $nparallel -learning_rate $lr -curriculum 1 -optim $optim | tee $log
 
