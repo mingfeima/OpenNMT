924ac954a04fe71ec5067f00d6f9be6a72854185
Mingfei Ma <mingfei.ma@intel.com>
2017-07-25 12:43:36 +0800

    update wmt15.sh

--- wmt15.sh
+++ wmt15.sh
@@ -6,5 +6,5 @@ lr=0.0064
 optim=adam
 log=wmt15_np${nparallel}_lr${lr}.txt
 
-th train.lua -data $path/wmt15-all-en-de-train.t7 -save_model $path/wmt15-all-en-de -nparallel $nparallel -learning_rate $lr -curriculum 1 -optim $optim | tee $log
+th train.lua -data $path/wmt15-all-en-de-train.t7 -save_model $path/wmt15-all-en-de -nparallel $nparallel -learning_rate $lr -curriculum 1 -optim $optim -save_every_epochs 0 | tee $log
 
